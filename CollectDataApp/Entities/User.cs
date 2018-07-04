21a78f6c06cde30f22f4952a4ce579be287f7dc9
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-05 01:40:33 +0300

    Added basic entities

---
+++ CollectDataApp/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectDataApp.Entities
+{
+    class User
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+        public List<Post> Posts { get; set; }
+        public List<ToDo> ToDos { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+}
