f1fc7a8bf9becc0a5a78b24da19f029b4afd1abf
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-05 12:36:25 +0300

    Added Settings class

---
+++ CollectDataApp/Settings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectDataApp
+{
+    static class Settings
+    {
+        public static string DataSourceUrl = "https://5b128555d50a5c0014ef1204.mockapi.io/";
+    }
+}
