e9b2825a5dbf11e35d8a03b023cc09ecbff9912e
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-06 10:42:47 +0300

    Added basic DataQueries

---
+++ CollectDataApp/Queries/DataQueries.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CollectDataApp.Entities;
+
+namespace CollectDataApp.Queries
+{
+    class DataQueries
+    {
+        private readonly List<User> _complexlyFilledUsers;
+
+        public DataQueries(List<User> complexlyFilledUsers)
+        {
+            _complexlyFilledUsers = complexlyFilledUsers;
+        }
+
+        public void GetNumberOfCommentsUnderUserPosts()
+        {
+
+        }
+
+        public void GetShortCommentsUnderUserPosts()
+        {
+
+        }
+
+        public void GetExecutedToDoByUser()
+        {
+
+        }
+
+        public void GetUsersAscWithToDoDesc()
+        {
+
+        }
+
+        public void GetUserStructure()
+        {
+
+        }
+
+        public void GetPostStructure()
+        {
+
+        }
+    }
+}
